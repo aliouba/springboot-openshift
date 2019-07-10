db1d37604e4e45930fec558e7efc74c66bb58d42
Mor Dioum <mordioum18@gmail.com>
2019-07-10 16:09:32 +0200

    première update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String getHome(){
-        return "Welcome to home page";
+        return "Welcome to home page mor dioum";
     }
 
     public static void main(String[] args) {
