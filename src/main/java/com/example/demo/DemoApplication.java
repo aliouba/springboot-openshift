b41f2608ad47c0d2bc7247112af796c9b5916068
mdioum <mordioum18@gmail.com>
2019-07-15 00:14:25 +0000

    update sonatel

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String getHome(){
-        return "Welcome to home page mor dioum samut";
+        return "Welcome Sonatel";
     }
 
     public static void main(String[] args) {
